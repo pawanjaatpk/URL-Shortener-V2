6574b842bd65c393e635a0e2ee1bb1980a3cd870
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2023-03-03 20:15:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ git clone https://github.com/pawanjaatpk/URL-Shortener-V2.git /URL-Shortener-V2
 cd /URL-Shortener-V2
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
-python3 main.py
+python3 bot.py
