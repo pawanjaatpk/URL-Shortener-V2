cbd1ca16e63fc4f1e0be195a93b7dc809be1f93f
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2023-03-31 20:58:11 +0530

    Update batch.py

--- plugins/batch.py
+++ plugins/batch.py
@@ -142,18 +142,49 @@ async def stop_button(c, m):
         logger.info("Batch Shortening Stopped Successfully 👍")
 
 @Client.on_message(filters.chat(channels_to_monitor) & ~filters.edited)
-async def convert_new_posts(c: Client, m: Message):
-    if not (m.media or m.text):
-        return
+async def convert_new_posts(c, channel_id, user_id):
+    user = await get_user(user_id)
+    user_method = user["method"]
+    try:
+        msg = await c.send_message(channel_id, "Checking for new messages...")
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        msg = await c.send_message(channel_id, "Checking for new messages...")
 
-    user = await get_user(m.from_user.id)
-    vld = await user_api_check(user)
-    if vld is not True:
-        return
+    last_message_id = None
 
-    async with lock:
+    while True:
         try:
-            await main_convertor_handler(message=m, type=user["method"], edit_caption=True, user=user)
-            await update_stats(m, user["method"])
+            messages = await c.get_history(channel_id, limit=50)
         except Exception as e:
-            logger.error(e)
+            print(f"Error getting message history: {e}")
+            await asyncio.sleep(5)
+            continue
+
+        messages = [msg for msg in messages if msg.message_id != last_message_id]
+
+        if len(messages) == 0:
+            await asyncio.sleep(10)
+            continue
+
+        messages.reverse()
+
+        for message in messages:
+            last_message_id = message.message_id
+
+            if message.media or message.text:
+                try:
+                    await main_convertor_handler(message=message, type=user_method, edit_caption=True, user=user)
+
+                    # Shorten the link using a URL shortener
+                    if user["shorten_links"]:
+                        url = f"https://t.me/{c.me.username}/{message.chat.id}/{message.message_id}"
+                        short_url = await temp.shorten_url(url)
+                        if short_url:
+                            await message.reply_text(short_url)
+
+                    await update_stats(message, user_method)
+                except Exception as e:
+                    print(f"Error processing message: {e}")
+
+            await asyncio.sleep(1)
